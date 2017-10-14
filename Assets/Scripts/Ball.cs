e98a8334dc5fefd2dde819c957018160d4def5e7
Jon Townsend <jon.townsend@limelyte.com>
2017-10-13 21:12:36 -0700

    Pins, ball, and camera
    Added pins and ball that collide together
    Camera tracks behind the ball going down the lane and stops before getting to the headpin.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().velocity = new Vector3 (0f, 0f, 200f);
+		this.GetComponent<AudioSource> ().Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
