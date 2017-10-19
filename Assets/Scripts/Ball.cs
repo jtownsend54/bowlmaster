253f5467af185700d5a33fd53112790bc1ab0bdc
Jon Townsend <jon.townsend@limelyte.com>
2017-10-19 06:49:11 -0700

    Add manual ball launch
    *EventTrigger added to touch panel
    *Launch script with start and end added that is used by event trigger
    *Ball script refactored to have a Launch method.
    *Launch script calculated velocity using mouse position and time, then calls ball launch.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-	public Vector3 velocity;
+	public Vector3 defaultVelocity;
+	private Rigidbody ballBody;
 
 	// Use this for initialization
 	void Start () {
-		this.GetComponent<Rigidbody>().velocity = velocity;
-		this.GetComponent<AudioSource> ().Play ();
+		ballBody = this.GetComponent<Rigidbody> ();
+		ballBody.useGravity = false;
+
+		//Launch (defaultVelocity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void Launch(Vector3 velocity) {
+		ballBody.useGravity = true;
+		ballBody.velocity = velocity;
+		this.GetComponent<AudioSource> ().Play ();
 	}
 }
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private float startTime;
+	private float endTime;
+
+	Ball ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Ball> ();
+	}
+
+	// Store position and time
+	public void DragStart() {
+		startPos = Input.mousePosition;
+		startTime = Time.time;
+	}
+
+	public void DragEnd() {
+		endPos = Input.mousePosition;
+		endTime = Time.time;
+		Vector3 change;
+
+		change.x = endPos.x - startPos.x;
+		change.y = endPos.z - startPos.z;
+		change.z = endPos.y - startPos.y;
+
+		//ball.GetComponent<Rigidbody> ().velocity = (endPos - startPos) / (endTime - startTime);
+		ball.Launch (change / (endTime - startTime));
+	}
+}
