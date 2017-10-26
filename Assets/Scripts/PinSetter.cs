7ed71a67929b1ffb73354c274b8ab69bc70dd734
Jon Townsend <jon.townsend@limelyte.com>
2017-10-26 06:44:54 -0700

    Added text that displays the standing pin count
    *Added emtpy cube around swiper and pins to be used later
    *Moved directional light into Lane, moved top camera into PinSetter

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+	GameObject pinCount;
+
+	// Use this for initialization
+	void Start () {
+		pinCount = GameObject.Find("PinCount");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pinCount.GetComponent<Text> ().text = CountStanding ().ToString();
+	}
+
+	int CountStanding() {
+		int count = 0;
+
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin> ()) {
+			if (pin.isStanding ()) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
