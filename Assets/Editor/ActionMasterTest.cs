77f1950ae7fdbe83a6d6457d32a3edce37e3212c
Jon Townsend <jon.townsend@limelyte.com>
2017-12-05 06:12:44 -0800

    Created more tests for various bowling actions

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 [TestFixture]
 public class ActionMasterTest {
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
 
 	[Test]
 	public void PassingTest () {
@@ -17,5 +18,28 @@ public class ActionMasterTest {
 	{
 		ActionMaster actionMaster = new ActionMaster ();
 		Assert.AreEqual(endTurn, actionMaster.Bowl (10));
+	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy()
+	{
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual(tidy, actionMaster.Bowl (8));
+	}
+
+	[Test]
+	public void T03SpareReturnsEndTurn()
+	{
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual(tidy, actionMaster.Bowl (8));
+		Assert.AreEqual(endTurn, actionMaster.Bowl (2));
+	}
+
+	[Test]
+	public void T04Bowl3And4ReturnsEndTurn()
+	{
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual(tidy, actionMaster.Bowl (3));
+		Assert.AreEqual(endTurn, actionMaster.Bowl (4));
 	}
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,13 +6,32 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+	private int bowlCount = 1;
+	private int[] bowls = new int[21];
+
 	public Action Bowl(int pins) {
 		if (pins > 10 || pins < 0) {
 			throw new UnityException ("Pins is out of bounds. Got " + pins);
 		}
 
+		bowls [bowlCount - 1] = pins;
+			
+		// Strike
 		if (pins == 10) {
+			bowlCount += 2;
 			return Action.EndTurn;
+		}
+
+		// End frame and there are no pins left. -2 is finding the previous score in the frame
+		if (bowlCount % 2 == 0) {
+			bowlCount ++;
+			return Action.EndTurn;
+		}
+
+		// Mid Frame
+		if (bowlCount % 2 != 0) {
+			bowlCount++;
+			return Action.Tidy;
 		}
 
 		throw new UnityException ("Ran out of actions, not sure what to do");
