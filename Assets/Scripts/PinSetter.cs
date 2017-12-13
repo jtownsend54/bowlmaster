dd1ef83031f3f40227090bb5f38aa8c123b75bc0
Jon Townsend <jon.townsend@limelyte.com>
2017-12-13 06:49:52 -0800

    Add ActionMaster to determine the animation
    *Fixed some resulting issues with resetting and the ball being reset to early.

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -10,14 +10,17 @@ public class PinSetter : MonoBehaviour {
 	public GameObject pinPrefab;
 	public ScoreMaster scoreMaster;
 
+	private ActionMaster actionMaster;
 	private float lastChangeTime;
 	private bool ballEnteredBox = false;
+	private int standingBeforeBallIsThrown;
 	private Ball ball;
 
 	// Use this for initialization
 	void Start () {
 		pinCount = GameObject.Find("PinCount").GetComponent<Text> ();
 		ball = GameObject.FindObjectOfType<Ball> ();
+		actionMaster = new ActionMaster ();
 	}
 	
 	// Update is called once per frame
@@ -30,6 +33,8 @@ public class PinSetter : MonoBehaviour {
 
 	void OnTriggerEnter(Collider collider) {
 		if (collider.gameObject.GetComponent<Ball>()) {
+			standingBeforeBallIsThrown = CountStanding ();
+			lastStandingCount = -1;
 			pinCount.color = Color.red;
 			ballEnteredBox = true;
 		}
@@ -57,6 +62,7 @@ public class PinSetter : MonoBehaviour {
 
 	void CheckStanding() {
 		int count = CountStanding ();
+		int pinsKnockedOver = standingBeforeBallIsThrown - count;
 
 		if (lastStandingCount != count) {
 			lastStandingCount = count;
@@ -66,8 +72,10 @@ public class PinSetter : MonoBehaviour {
 			
 		// If pin count has remained the same for at least 3 seconds
 		if (Time.time - lastChangeTime >= 3) {
-			scoreMaster.UpdateScore (lastStandingCount);
+			//scoreMaster.UpdateScore (lastStandingCount);
 			PinsHaveSettled ();
+			ActionMaster.Action action = actionMaster.Bowl (pinsKnockedOver);
+			doAnimation (action);
 		}
 	}
 
@@ -75,7 +83,7 @@ public class PinSetter : MonoBehaviour {
 		ball.Reset ();
 		pinCount.color = Color.green;
 		ballEnteredBox = false;
-		lastStandingCount = -1;
+
 	}
 
 	public void RaisePins() {
@@ -94,6 +102,15 @@ public class PinSetter : MonoBehaviour {
 		Destroy(GameObject.Find("Pins"));
 
 		pins = Instantiate (pinPrefab, new Vector3 (0, 30, 1829), Quaternion.identity);
+	}
+
+	private void doAnimation(ActionMaster.Action action) {
+		if (action == ActionMaster.Action.Tidy) {
+			gameObject.GetComponent<Animator> ().SetTrigger ("triggerTidy");
+		} else {
+			lastStandingCount = 10;
+			gameObject.GetComponent<Animator> ().SetTrigger ("triggerRenew");
+		}
 	}
 }
 
